7f50c998c2de8fdd6ddcb4de0bd9a857a696af41
riz-ahmed <rzwn.hmd@gmail.com>
2020-11-07 04:05:10 +0100

    week 2 assignments completed

--- Octave_Programming_Assignments/week_2/machine-learning-ex1/machine-learning-ex1/ex1/computeCost.m
+++ Octave_Programming_Assignments/week_2/machine-learning-ex1/machine-learning-ex1/ex1/computeCost.m
@@ -13,9 +13,9 @@ J = 0;
 % Instructions: Compute the cost of a particular choice of theta
 %               You should set J to the cost.
 
-
-
-
+h = X*theta;				% prediction
+sq_errors = (h - y).^2;		% square of errors
+J = (1/(2*m))*sum(sq_errors);			% cost function
 
 % =========================================================================
 
--- Octave_Programming_Assignments/week_2/machine-learning-ex1/machine-learning-ex1/ex1/plotData.m
+++ Octave_Programming_Assignments/week_2/machine-learning-ex1/machine-learning-ex1/ex1/plotData.m
@@ -21,5 +21,7 @@ figure; % open a new figure window
 
 
 % ============================================================
-
+plot(x,y, 'rx', 'MarkerSize', 10)
+xlabel('population')
+ylabel('revenue')
 end
