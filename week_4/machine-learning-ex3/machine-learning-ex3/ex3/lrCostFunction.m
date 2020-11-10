331afd03f8139875ba23f4aaab47dd3bbead1c06
riz-ahmed <rzwn.hmd@gmail.com>
2020-11-10 22:34:02 +0100

    week 4 tasks completed till oneVsAll function

--- week_4/machine-learning-ex3/machine-learning-ex3/ex3/lrCostFunction.m
+++ week_4/machine-learning-ex3/machine-learning-ex3/ex3/lrCostFunction.m
@@ -38,9 +38,11 @@ grad = zeros(size(theta));
 
 z = (X * theta);
 h_x = sigmoid(z);
-J = (1/m)*sum((-y.*log(h_x)) - ((1 - y).*log(1 - h_x)));
+regularized_term = (lambda/(2*m))*sum(theta(2:end).^2);
+J = (1/m)*sum((-y.*log(h_x)) - ((1 - y).*log(1 - h_x))) + regularized_term;
 
-grad = (1/m)*(X'*(h_x - y));
+grad(1) = (1/m)*sum((h_x - y).*X(:,1));
+grad(2:end) = (1/m)*(transpose(X(:,2:end))*(h_x - y)) + (lambda/m)*theta(2:end);
 
 
 
